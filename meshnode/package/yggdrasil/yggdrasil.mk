e00f5a871618de31c0f086e2d8df4a25d1e67b07
William Wennerstr?m <william@willeponken.me>
2019-04-02 22:01:28 +0200

    buildroot+meshnode: fix GO111MODULE issue and initial mender

--- meshnode/package/yggdrasil/yggdrasil.mk
+++ meshnode/package/yggdrasil/yggdrasil.mk
@@ -4,24 +4,22 @@
 #
 ################################################################################
 
-YGGDRASIL_VERSION = 0.3.3
+YGGDRASIL_VERSION = 0.3.5
 YGGDRASIL_SITE = $(call github,yggdrasil-network,yggdrasil-go,v$(YGGDRASIL_VERSION))
 YGGDRASIL_LICENSE = LGPL-3.0
 YGGDRASIL_LICENSE_FILES = LICENSE
 
-YGGDRASIL_LDFLAGS=-X $(YGGDRASIL_SRC_SUBDIR)/src/yggdrasil.buildName=yggdrasil \
-		  -X $(YGGDRASIL_SRC_SUBDIR)/src/yggdrasil.buildVersion=$(YGGDRASIL_VERSION) \
-		  -s -w
+YGGDRASIL_LDFLAGS = -X $(YGGDRASIL_SRC_SUBDIR)/src/yggdrasil.buildName=yggdrasil \
+		    -X $(YGGDRASIL_SRC_SUBDIR)/src/yggdrasil.buildVersion=$(YGGDRASIL_VERSION) \
+		    -s -w
+YGGDRASIL_GO_ENV = GO111MODULE=on
 
 YGGDRASIL_BUILD_TARGETS = cmd/yggdrasil cmd/yggdrasilctl
 YGGDRASIL_INSTALL_BINS = yggdrasil yggdrasilctl
 
-HOST_GO_TARGET_ENV += GO111MODULE=on
-HOST_GO_MAKE_ENV += GO111MODULE=on
-
 define YGGDRASIL_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 \
-		$(YGGDRASIL_PKGDIR)/files/yggdrasil.init \
+		$(YGGDRASIL_SRC_PATH)/contrib/busybox-init/S42yggdrasil \
 		$(TARGET_DIR)/etc/init.d/S42yggdrasil
 endef
 
